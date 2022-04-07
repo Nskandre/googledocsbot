155d6364ba55aea5dafca3fbc69c524f798f281b
Andrei <rinf@mail.ru>
2022-04-07 21:03:34 +0300

    Fix private key

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ CREDENTIALS = {
   "type": "service_account",
   "project_id": "primal-drake-345119",
   "private_key_id": os.getenv("PRIVATE_KEY_ID", ""),
-  "private_key": os.getenv("PRIVATE_KEY", ""),
+  "private_key": os.getenv("PRIVATE_KEY", "").replace('\\n', '\n'),
   "client_email": "google-sheets-api@primal-drake-345119.iam.gserviceaccount.com",
   "client_id": os.getenv("CLIENT_ID", ""),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
